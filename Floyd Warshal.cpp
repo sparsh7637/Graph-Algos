ac1333d1968eea25fe9d567e856561dedc654f0a
sparsh_gualti <91118234+sparsh7637@users.noreply.github.com>
2024-05-22 19:49:00 +0530

    Add files via upload

---
+++ Floyd Warshal.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&mat){
+	    int n=mat.size();
+	    for(int i=0; i<n; i++){
+	        for(int j=0; j<n; j++){
+	            if(mat[i][j]==-1) mat[i][j]=1e9;
+	        }
+	    }
+	    
+	    
+	    for(int k=0; k<n; k++){
+	        for(int i=0; i<n; i++){
+	            for(int j=0; j<n; j++){
+	                mat[i][j]=min(mat[i][j], mat[i][k]+mat[k][j]);
+	            }
+	        }
+	    }
+	    
+	    for(int i=0; i<n; i++){
+	        for(int j=0; j<n; j++){
+	            if(mat[i][j]==1e9) mat[i][j]=-1;
+	        }
+	    }
+	    
+	}
+};
